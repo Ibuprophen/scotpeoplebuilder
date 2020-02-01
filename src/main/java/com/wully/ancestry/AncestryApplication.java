f7b6a9ccdb4e2a1f9cfd79df4f4f75585c3fb503
William Robb <william_robb9@hotmail.com>
2020-02-01 12:27:43 +0000

    initial

---
+++ src/main/java/com/wully/ancestry/AncestryApplication.java
@@ -0,0 +1,13 @@
+package com.wully.ancestry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncestryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AncestryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wully/ancestry/AncestryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wully.ancestry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncestryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
